e4bc0575d3f9a641d866d08648ba57d8af604e3c
eakmv <akimov890@mail.ru>
2019-12-14 23:16:19 +0300

    Update 6kuy_take_a_number_and_sum_its.py

--- 6kuy_take_a_number_and_sum_its.py
+++ 6kuy_take_a_number_and_sum_its.py
@@ -1,4 +1,8 @@
-def sum_dig_pow(a, b): # range(a, b + 1) will be studied by the function
+"""
+https://www.codewars.com/kata/5626b561280a42ecc50000d1
+"""
+
+def sum_dig_pow(a, b):
     number_list = []
     start = a
     while start <= b:
