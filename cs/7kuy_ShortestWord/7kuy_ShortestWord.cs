6018615f67e738cd45db8032cbce4f42329f2996
Eugene Akimov <eugene.akimov@solutionsfactory.at>
2020-11-20 17:54:19 +0300

    change folders structure, add cs folder

---
+++ cs/7kuy_ShortestWord/7kuy_ShortestWord.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class Kata
+{
+    static public void Main()
+    {
+        int size = Regexp("https://v1810devsfp6b2ca5deed6a5b3a7devaos.cloudax.dynamics.com/?cmp=USMF&mi=DefaultDashboard");
+        Console.WriteLine(size);
+    }
+
+    public static int Regexp(string s)
+  {
+      string pattern = @"(\w)*devaos.cloudax.dynamics.com/";
+      if (Regex.IsMatch(s, pattern, RegexOptions.IgnoreCase))
+        return 1;
+      else
+        return 0;
+  }
+}
