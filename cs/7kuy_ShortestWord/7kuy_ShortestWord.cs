4c9c3ea064690f78aba2b5a763b00279d959fbc7
Eugene Akimov <eugene.akimov@solutionsfactory.at>
2020-11-25 15:54:38 +0300

    upd.

--- cs/7kuy_ShortestWord/7kuy_ShortestWord.cs
+++ cs/7kuy_ShortestWord/7kuy_ShortestWord.cs
@@ -1,20 +1,16 @@
-﻿using System;
-using System.Text.RegularExpressions;
-
-public class Kata
+﻿public class Kata
 {
-    static public void Main()
-    {
-        int size = Regexp("https://v1810devsfp6b2ca5deed6a5b3a7devaos.cloudax.dynamics.com/?cmp=USMF&mi=DefaultDashboard");
-        Console.WriteLine(size);
-    }
-
-    public static int Regexp(string s)
+  public static int FindShort(string s)
   {
-      string pattern = @"(\w)*devaos.cloudax.dynamics.com/";
-      if (Regex.IsMatch(s, pattern, RegexOptions.IgnoreCase))
-        return 1;
-      else
-        return 0;
+      string[] words = s.Split(' ');
+      int lowestLenth = int.MaxValue;
+      foreach(var item in words)
+        {
+            if (item.Length < lowestLenth)
+            {
+                lowestLenth = item.Length;
+            }
+        }
+      return lowestLenth;
   }
 }
